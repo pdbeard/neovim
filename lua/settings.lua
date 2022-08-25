8991da461e6e28983f8b0926b36d81999e839460
Patrick <patb1286@gmail.com>
2022-08-25 10:49:34 +0100

    Setting options

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,8 +1,13 @@
 local o = vim.o      --Global Options
 local wo = vim.wo    --Window-local Options
 local bo = vim.bo    --Buffer-local Options 
-		
+
+
+
+-----------------		
 -- global options
+-----------------
+
 o.swapfile = true
 o.dir = '/tmp'
 o.smartcase = true
@@ -13,9 +18,75 @@ o.ignorecase = true
 o.scrolloff = 12
 -- ... snip ... 
 
+
+
+-----------------
+-- mine
+-----------------
+
+o.showtabline = 2
+o.scrolloff = 5
+o.mouse = 'a'
+o.backupcopy = 'yes'
+o.undolevels = 1000
+-- shortmess options?
+o.showmode = true        -- nv-ide has false but still shows? Check bar settings?
+o.hidden = true
+o.splitright = true
+o.splitbelow = true
+o.wrapscan = true
+o.backup = false         -- eh?
+o.writebackup = false    -- eh?
+o.showcmd = true
+o.showmatch = true
+o.ignorecase = true
+o.hlsearch = true
+o.smartcase = true
+o.errorbells = false
+o.joinspaces = false
+o.title = true
+o.lazyredraw = true      -- eh?
+o.encoding = 'UTF-8'
+-- o.completeopt = { 'menu', 'menuone', 'noselect' }
+o.clipboard = 'unnamedplus'
+--o.listchars = { tab = ">>>", trail = "·", precedes = "←", extends = "→",eol = "↲", nbsp = "␣" }
+o.laststatus = 3 
+o.timeoutlen = 500
+
+
+
+-----------------------
 -- window-local options
+-----------------------
+
 wo.number = true
 wo.wrap = false
+wo.colorcolumn = "+1"
+wo.foldmethod = 'indent'
+wo.foldlevel = 1
+wo.list = false
+wo.foldnestmax = 10
+wo.signcolumn = 'yes'
+wo.relativenumber = true
+wo.foldenable = false
+wo.cursorline = false
 
+
+-----------------------
 -- buffer-local options
+-----------------------
+
 bo.expandtab = true
+bo.fileformat = 'unix'
+bo.tabstop = 2
+--bo.spelllang = 'en'
+bo.softtabstop = 2
+bo.swapfile = false
+bo.undofile = false
+bo.expandtab = true
+bo.shiftwidth = 2
+
+
+
+
+
